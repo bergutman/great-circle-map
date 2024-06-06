1549f37e0774b3fc92a0b537716ba8abc6df8197
Ber Gutman <bergutman@pm.me>
2024-06-06 13:50:48 +0900

    path fixes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/great-circle-map/'
   },
   module: {
     rules: [
